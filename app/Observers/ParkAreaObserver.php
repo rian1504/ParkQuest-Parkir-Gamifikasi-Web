bd880f09c5943e49d2f73d80af9f97eff5b37c77
rian1504 <adaadi3526@gmail.com>
2024-11-08 15:53:08 +0700

    Update Feat: Park Area Admin Memperbaiki CRUD pada gambar

---
+++ app/Observers/ParkAreaObserver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\ParkArea;
+use Illuminate\Support\Facades\Storage;
+
+class ParkAreaObserver
+{
+    public function saved(ParkArea $parkArea): void
+    {
+        if ($parkArea->isDirty('park_image')) {
+            Storage::disk('public')->delete($parkArea->getOriginal('park_image'));
+        }
+    }
+
+    public function deleted(ParkArea $parkArea): void
+    {
+        if (! is_null($parkArea->park_image)) {
+            Storage::disk('public')->delete($parkArea->park_image);
+        }
+    }
+}
