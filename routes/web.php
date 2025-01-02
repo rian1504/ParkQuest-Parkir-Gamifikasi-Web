8a643010a9c1a0089ee1e3145dbd75e2d4796065
rian1504 <adaadi3526@gmail.com>
2025-01-02 18:08:41 +0700

    Feat: Artisan Manual Membuat fungsi untuk memanggil Artisan Command secara manual melalui link

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ Route::get('/', function () {
 //     \Illuminate\Support\Facades\Artisan::call('storage:link');
 //     echo 'Symlink berhasil';
 // });
+
+// Route::get('/updateRank', function () {
+//     \Illuminate\Support\Facades\Artisan::call('update:user-ranks');
+//     echo 'Update rank berhasil';
+// });
+
+// Route::get('/resetMisi', function () {
+//     \Illuminate\Support\Facades\Artisan::call('missions:reset-weekly');
+//     echo 'Reset misi berhasil';
+// });
